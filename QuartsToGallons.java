dc0f463f5b402b4378e2c7865a8b69471c9a936f
ljsnell <ljsnell7@gmail.com>
2022-07-30 15:50:15 -0500

    Update QuartsToGallons.java
    It works.

--- QuartsToGallons.java
+++ QuartsToGallons.java
@@ -1,5 +1,9 @@
 public class QuartsToGallons {
     public static void main(String[] args) {
+        int quarts_in_gallon = 4;
+        int quarts_needed_for_paint_job = 18;
 
+        System.out.println("You'll need " + quarts_needed_for_paint_job / quarts_in_gallon +
+                " gallons for this job plus " + quarts_needed_for_paint_job % quarts_in_gallon + " quarts");
     }
 }
