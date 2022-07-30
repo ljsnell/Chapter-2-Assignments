2eb2bffbdfd0b0b5dbf22bfc15d0ffa03f22dcd2
ljsnell <ljsnell@users.noreply.github.com>
2022-07-30 15:36:30 -0500

    Template (#1)
    * Initial Template for assignments
    
    ^
    
    * Update .gitignore
    
    Cleanup gitignore.

---
+++ ChiliToGo.java
@@ -0,0 +1,11 @@
+public class ChiliToGo {
+    public static void main(String[] args) {
+        // Accept adult meals count
+
+        // Accept children's meals count
+
+        // Work your magic
+
+        // Display money collected for adult's, children, and all meals on separate console lines.
+    }
+}
