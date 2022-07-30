7b556dff6a85824a7f435a18532599d22af94415
ljsnell <ljsnell@users.noreply.github.com>
2022-07-30 15:53:29 -0500

    Update QuartsToGallons.java

--- QuartsToGallons.java
+++ QuartsToGallons.java
@@ -1,5 +1,8 @@
 public class QuartsToGallons {
     public static void main(String[] args) {
-
+        int quarts_in_gallon = 4;
+        int quarts_needed_for_paint_job = 18;
+        
+        // Do you thing
     }
 }
